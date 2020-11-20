73f595d6d1bf609e271cebc459496ed4b1c436ad
Arun Vijayarengan <arunprabu.v@hotmail.com>
2020-11-20 13:30:25 +0530

    angular project with menu comp

---
+++ contact-mgr-nov2020/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [ AppComponent ] // AppModule should bootstrap AppComponent
+})
+export class AppModule { }
